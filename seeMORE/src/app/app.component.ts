239be0967dedeb24adc777ed503e1a2208045a3a
ANASS <48458319+ouardini@users.noreply.github.com>
2019-10-19 13:47:15 +0100

    Update app.component.ts

--- seeMORE/src/app/app.component.ts
+++ seeMORE/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {  NavController } from '@ionic/angular' ;
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public alertController: AlertController,
+    public navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -24,4 +28,29 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Alert!!',
+      message: 'Are you sure you want to sign out ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            this.navCtrl.navigateForward('/home') ;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  
 }
+
